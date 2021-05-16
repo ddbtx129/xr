ad4b632ab0715c127361e26364f4b5cefcc53390
ddbtx129 <63324152+ddbtx129@users.noreply.github.com>
2021-05-17 02:18:48 +0900

    Add files via upload

--- js/share/component/b-fireworks/fireworks.js
+++ js/share/component/b-fireworks/fireworks.js
@@ -278,7 +278,7 @@
 				this.data.color = this.randomColor();
 			}
 			console.log(path);
-			console.log(fireball);
+			console.log(this.data.texture);
 			this.pCount = { min: Number(this.data.pCount[0]), max: Number(this.data.pCount[1]) };
 			this.bVelocity = { min: Number(this.data.bVelocity[0]), max: Number(this.data.bVelocity[1]) };
 
