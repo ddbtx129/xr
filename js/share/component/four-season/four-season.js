ca7ee562b93a796bdbacaa7039b908f7710a5c09
ddbtx129 <63324152+ddbtx129@users.noreply.github.com>
2021-05-24 23:45:54 +0900

    Add files via upload

--- js/share/component/four-season/four-season.js
+++ js/share/component/four-season/four-season.js
@@ -158,8 +158,8 @@
         },
 
         tick: function (time, dt) {
-            //let element = this.el;
-            //element.parentNode.removeChild(element);
+            let element = this.el;
+            element.parentNode.removeChild(element);
         }
     });
 
